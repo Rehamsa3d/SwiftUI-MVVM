a7472d1fddb0bb6514607cb260a3654c970e1a21
rehamsaad <rehamsd2@gmail.com>
2025-12-29 21:01:13 +0200

    adding custom splashView as a root view for my app

---
+++ SwiftUI+MVVM/App/AppRootView/AppRootView.swift
@@ -0,0 +1,8 @@
+//
+//  AppRootView.swift
+//  SwiftUI+MVVM
+//
+//  Created by Reham on 29/12/2025.
+//
+
+import Foundation
---
+++ SwiftUI+MVVM/App/AppRootView/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  SwiftUI+MVVM
+//
+//  Created by Reham on 29/12/2025.
+//
+
+import Foundation
